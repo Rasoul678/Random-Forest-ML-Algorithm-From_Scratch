88c673384777e83d211a9b6288940f18513f62d0
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2025-05-22 19:07:24 +0330

    :zap: Analyzing! :)

--- RandomForest.py
+++ RandomForest.py
@@ -17,8 +17,9 @@ class RandomForestClassifier:
     def fit(self, X, y):
         self.trees = []
         for _ in range(self.n_trees):
-            idxs = np.random.choice(len(X), len(X), replace=True)
-            X_sample, y_sample = X[idxs], y[idxs]
+            # Bootstrap aggregating (Bagging)
+            ides = np.random.choice(len(X), len(X), replace=True)
+            X_sample, y_sample = X[ides], y[ides]
             tree = DecisionTree(max_depth=self.max_depth,
                                 min_samples_split=self.min_samples_split,
                                 n_features=self.n_features, criterion=self.criterion)
@@ -26,7 +27,9 @@ class RandomForestClassifier:
             self.trees.append(tree)
 
     def predict(self, X):
-        tree_preds = np.array([tree.predict(X) for tree in self.trees])
-        tree_preds = np.swapaxes(tree_preds, 0, 1)
-        y_pred = [Counter(row).most_common(1)[0][0] for row in tree_preds]
-        return np.array(y_pred)
+        tree_predictions = np.array([tree.predict(X) for tree in self.trees])
+        # print(f"Before: {tree_predictions}")
+        tree_predictions = np.swapaxes(tree_predictions, 0, 1)
+        # print(f"After swap: {tree_predictions}")
+        y_prediction = [Counter(row).most_common(1)[0][0] for row in tree_predictions]
+        return np.array(y_prediction)
